5fb41c16a227f0fb1b4723259abf2c47114607b4
Tudor Malene <tudor.malene@gmail.com>
2012-10-26 12:41:02 +0300

    heroku stuff

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -25,6 +25,8 @@ grails.project.dependency.resolution = {
         mavenLocal()
         mavenCentral()
 
+        flatDir(name:'local-plgns', dir:'local-plgns')
+
         // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
         //mavenRepo "http://snapshots.repository.codehaus.org"
         //mavenRepo "http://repository.codehaus.org"
