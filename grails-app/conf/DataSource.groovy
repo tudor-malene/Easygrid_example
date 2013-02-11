75f0d71969af69411ac512cdf76c7e5165f8e803
Tudor Malene <tudor.malene@gmail.com>
2013-02-11 19:42:33 +0200

    -datasource name

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -27,7 +27,7 @@ environments {
     production {
         dataSource {
             dbCreate = "create-drop"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            url = "jdbc:h2:easygridExampleDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
             pooled = true
             properties {
                maxActive = -1
